c8a0dc9a7515d6f9b4b8c0bef3730715e142ef28
BlobbleWobble <75754640+BlobbleWobble@users.noreply.github.com>
2022-06-30 07:08:49 +0700

    added shell script

---
+++ kelompok2pt2 .sh
@@ -0,0 +1,7 @@
+cd /etc
+echo 'DISTRIB_ID=Ubuntu
+DISTRIB_RELEASE=22.04
+DISTRIB_CODENAME=jammy
+DISTRIB_DESCRIPTION="pegalinux Edisi Suyono"' > lsb-release
+echo "pegalinux Edisi Suyono \n \l" > issue
+echo "pegalinux Edisi Suyono 22.04 LTS" > issue.net
